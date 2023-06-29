d6421c756e66bddcdb7432da66f030c1ad5f0cc2
cyj <itnaupna@gmail.com>
2023-06-29 16:27:08 +0900

    first commit

---
+++ src/main/java/com/bit/wepli/WepliApplication.java
@@ -0,0 +1,13 @@
+package com.bit.wepli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WepliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WepliApplication.class, args);
+	}
+
+}
