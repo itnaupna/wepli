7ba63b3d5f52da234321fe7c35744bd7ddcb44b7
Lekahelis <itnaupna@gmail.com>
2023-07-02 14:03:55 +0900

    FIX : HTTP to HTTPS Redirect, proxy port number

--- src/main/java/com/bit/config/SslConfig.java
+++ src/main/java/com/bit/config/SslConfig.java
@@ -4,16 +4,16 @@ import org.apache.catalina.Context;
 import org.apache.catalina.connector.Connector;
 import org.apache.tomcat.util.descriptor.web.SecurityCollection;
 import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableConfigurationProperties
 public class SslConfig {
-
+    @Bean
     public ServletWebServerFactory servletContainer() {
         TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
             @Override
@@ -25,26 +25,18 @@ public class SslConfig {
                 securityConstraint.addCollection(collection);
                 context.addConstraint(securityConstraint);
             }
-            // }
         };
+
         tomcat.addAdditionalTomcatConnectors(createSslConnector());
         return tomcat;
     }
 
-    @Value("${server.port.http}")
-    private int httpPort;
-
-    @Value("${server.port}")
-    private int httpsPort;
-
-    private Connector createSslConnector(){
+    private Connector createSslConnector() {
         Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-        connector.setPort(httpPort);
+        connector.setPort(80);
         connector.setScheme("http");
         connector.setSecure(false);
-        connector.setRedirectPort(httpsPort);
-
-
+        connector.setRedirectPort(443);
         return connector;
     }
 }
