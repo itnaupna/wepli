0842adbf4d6a08edebb805e99001738146c266c0
helloa1109 <helloa1109@naver.com>
2023-07-03 12:23:23 +0900

    New Front End Setting

--- front/src/App.js
+++ front/src/App.js
@@ -1,35 +1,16 @@
-import logo from './logo.svg';
+
 import './App.css';
-import {useEffect, useState} from 'react';
-import axios from 'axios';
-import PlayListMain from "./PlayListMain/PlayListMain";
+
+
+import MainSection from "./main/MainJs/MainSection";
 
 function App() {
-  const [msg, setMsg] = useState('fail');
-  
-  useEffect(()=>{
-    axios.get("/api/test")
-    .then(res=>setMsg(res.data));
-  },[])
+
 
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {msg}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      <PlayListMain/>
-      </header>
+      <MainSection/>
     </div>
   );
 }
