d47a2ba743f08a5d33e22ada7e42b61e13edc70b
helloa1109 <helloa1109@naver.com>
2023-07-19 17:18:16 +0900

    마이페이지 ( 회원탈퇴 ) 소셜로그인 ( 카카오: url 수정)

--- front/src/MypageModal/EmailConfirmModal.js
+++ front/src/MypageModal/EmailConfirmModal.js
@@ -1,13 +1,44 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "./css/EmailConfirm.css";
 import backarrow from "./svg/backarrow.svg";
 import btnarrow from "./svg/btnarrow.svg";
 import logo from "./photo/weplieonlylogoonlylogo.png";
+import axios from "axios";
 function EmailConfirmModal({setisEmailConfirmModalOpen}) {
+
+    const [verifyKey, setVerifyKey] = useState('');
+    const [verifyType, setVerifyType] = useState(0);
+    const [resultRV, setResultRV] = useState(false);
+    const [verifyCode, setVerifyCode] = useState('');
+    const [resultVerify, setResultVerify] = useState(false);
+
+    const handleRequestCode = async () => {
+        const url = "/api/lv1/m/requestcode";
+        try{
+            const res = await axios
+                .post(url,{key: verifyKey });
+            setResultRV(res.data);
+            alert("굿")
+        }catch (error){
+            console.log(error);
+        }
+    }
+
+    const handleVerifyCode = async ()=>{
+        const url = "/api/lv1/m/verifycode";
+        try{
+            const res = await axios.post(url,{key:verifyKey,code:verifyCode});
+            setResultVerify(res.data);
+            alert("good");
+        }catch(error){
+            alert(error);
+        }
+    }
 
     const closeEmailConfirmModal = async () => {
         await setisEmailConfirmModalOpen(false);
     }
+
 
     return (
         <div>
@@ -32,23 +63,26 @@ function EmailConfirmModal({setisEmailConfirmModalOpen}) {
                         <div className="emailconfirmmodalcentertext">이메일 본인인증</div>
                     </div>
                     <div className="emailconfirmmodalemailinputgro">
-                        <input type={'email'} className="emailconfirmmodalemailinput"></input>
+                        <input type={'email'} className="emailconfirmmodalemailinput"
+                               value={verifyKey} onChange={(e) => setVerifyKey(e.target.value)}></input>
                     </div>
                     <div className="emailconfirmemailbtngroup">
                         <div className="emailconfirmsendbtn" />
-                        <div className="emailconfirmemailbtntext">전송</div>
+                        <button type={'button'} className="emailconfirmemailbtntext"
+                         onClick={handleRequestCode}>전송</button>
                     </div>
                     <div className="emailconfirmmodalemailinputgro">
                         <input type={'text'} className="emailconfirmmodalemailinput"></input>
                     </div>
                     <div className="emailconfirmemailbtngroup">
                         <div className="emailconfirmsendbtn" />
-                        <div className="emailconfirmemailbtntext">확인</div>
+                        <button type={'button'} className="emailconfirmemailbtntext"
+                        onClick={handleVerifyCode}>확인</button>
                     </div>
                     <div className="emailconfirmmodalbtngroup">
                         <div className="emailconfirmmypagebtn">
                             <div className="emailconfirmmodalbtnrectangle" />
-                            <div className="emailconfirmmodalbtntext">본인인증</div>
+                            <button type={'button'} className="emailconfirmmodalbtntext">본인인증</button>
                         </div>
                         <img
                             className="emailconfirmmodalbtnarrow-icon"
--- front/src/main/MainWrapper4.js
+++ front/src/main/MainWrapper4.js
@@ -16,6 +16,15 @@ function MainWrapper4(props) {
                 setLikeTop3(res.data));
 
     },[]);
+
+
+    const profileimg = process.env.REACT_APP_BUCKET_URL;
+
+    // ?.은 ?.'앞’의 평가 대상이 undefined나 null이면 평가를 멈추고 undefined를 반환.
+    const img = likeTop3[0]?.img;
+    const img1 = likeTop3[1]?.img;
+    const img2 = likeTop3[2]?.img;
+
     return (
         <div className="mainwrapper4">
             <div className="mainwrapper4group">
@@ -42,7 +51,7 @@ function MainWrapper4(props) {
                                     <img
                                         className="mainwrapper4secondrankimg-icon"
                                         alt=""
-                                        src={albumcover}
+                                        src={`${profileimg}/playlist/${img1}`}
                                     />
                                 </div>
                                 <div className="mainwrapper4secondrankingtextg">
@@ -59,7 +68,7 @@ function MainWrapper4(props) {
                                     <img
                                         className="mainwrapper4firstrankimg-icon"
                                         alt=""
-                                        src={albumcover}
+                                        src={`${profileimg}/playlist/${img}`}
                                     />
                                 </div>
                                 <div className="mainwrapper4firstrankingtextgr">
@@ -76,7 +85,7 @@ function MainWrapper4(props) {
                                     <img
                                         className="mainwrapper4thirdrankimg-icon"
                                         alt=""
-                                        src={albumcover}
+                                        src={`${profileimg}/playlist/${img2}`}
                                     />
                                 </div>
                                 <div className="mainwrapper4thirdrankingtextgr">
