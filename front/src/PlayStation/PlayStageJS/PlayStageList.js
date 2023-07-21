abbd09712d2ca91841895df5b8931d822356f79c
SeokjinKim <jameskim0208@gmail.com>
2023-07-21 10:48:46 +0900

    "20230721StageListselect&SearchBar완성"

--- front/src/PlayStation/PlayStageJS/PlayStageList.js
+++ front/src/PlayStation/PlayStageJS/PlayStageList.js
@@ -5,7 +5,8 @@ import SLPFollowNextIcon from '../PlayStageImage/Icon/SLPFollowNextIcon.svg';
 import StageItemBig from "./StageItemBig";
 import ResultItem from "./ResultItem";
 import CreateStageModal from "./CreateStageModal.js";
-import axios from 'axios';
+import SearchBar from "./PlayStageSearchBar.js";
+import { Axios } from 'axios';
 
 function PlayStageList(props) {
   const [modalOpen, setModalOpen] = useState(false);
@@ -17,13 +18,41 @@ function PlayStageList(props) {
   useEffect(() => {
 
   }, []);
+  
+  //최신순(기본값) 인기순 토글 셀렉트
+  const [type1,setType1] = useState(0);
+  const [type2,setType2] = useState(0);
+  const [toggleOption1,setToggleOption1]=useState(["최신순","인기순"]);
+  const [toggleOption2,setToggleOption2]=useState(["제목","닉네임","장르","태그"]);
+  const [isOpen1,setIsOpen1] = useState(false);
+  const [isOpen2,setIsOpen2] = useState(false);
+  const toggle1Dropdown = () =>{
+    setIsOpen1(!isOpen1);
+  };
+  const toggle2Dropdown = () =>{
+    setIsOpen2(!isOpen2);
+  };
+  const SelectOption1 = (e) =>{
+    setType1(e.target.getAttribute("option"));
+  }
+  const SelectOption2 = (e) =>{
+    setType2(e.target.getAttribute("value"));
+  }
+  const reload = () =>{
+    window.location.replace("");
+  }
 
   return (
     <div className="slp">
       <div className="slptop">
         <div className="slpmystagewrapper">
           <StageItemBig />
-          
+          <div className="StageModalContainer">                
+                <button onClick={showModal} className="button button--nina button--round-l button--text-thick button--inverted" data-text="스테이지생성">
+                <span>스</span><span>테</span><span>이</span><span>지</span><span>생</span><span>성</span>
+                </button>
+                {modalOpen && <CreateStageModal setModalOpen={setModalOpen}/>}
+        </div>
         </div>
         <div className="slpfollowwrapper">
           <div className="slpfollowback">
@@ -46,11 +75,42 @@ function PlayStageList(props) {
       <div className="slpbottom">
         <div className="slpsearchwrapper">
           <div className="slpsortwrapper">
-            <div className="slpsort" />
+            <div className="slpsort">
+              <div className="slpsort-selectbox">
+                <div className="slpsort-selectboxbody" />
+                <svg 
+                width="24" 
+                height="24" 
+                viewBox="0 0 11 11" 
+                fill="none" xmlns="http://www.w3.org/2000/svg"
+                className="playlistmainsearchtoggle-icon"
+                >
+                  <path id="Intersect" d="M1.50972 2.35292C2.44299 4.70758 4.44138 9.36599 5.6433 9.36291C6.84522 9.35984 8.8249 4.69125 9.74872 2.33184C10.0015 1.68631 9.52123 1.00203 8.82811 1.00381L2.42499 1.02019C1.73187 1.02197 1.25438 1.70869 1.50972 2.35292Z" fill="#D7E0FF" stroke="#4147D5" stroke-width="1.5"/>
+                </svg>
+                <div className="slpsort-selectoption" onClick={toggle1Dropdown}>{
+                toggleOption1[type1]}
+                {isOpen1 && (
+                  <div className="slpsort-selectDropDownBody">
+                    <svg 
+                    width="24" 
+                    height="24" 
+                    viewBox="0 0 11 11" 
+                    fill="none" 
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="playlistmainsearchtoggleUP-icon"
+                    >
+                    <path id="Intersect" d="M9.75359 8.03251C8.82635 5.67547 6.83989 1.01196 5.63797 1.01196C4.43605 1.01196 2.44442 5.67547 1.51457 8.03251C1.26016 8.67739 1.73865 9.36289 2.43177 9.36289H8.83491C9.52803 9.36289 10.0073 8.67739 9.75359 8.03251Z" fill="#D7E0FF" stroke="#4147D5" stroke-width="1.5"/>
+                    </svg>
+                    <option className='slpsort-selectDropDown' onClick={SelectOption1} option={0}>최신순</option>
+                    <option className="slpsort-selectDropDown" onClick={SelectOption1} option={1}>인기순</option>
+                  </div>
+                )}
+                </div>
+              </div>  
+            </div>
           </div>
-          <div className="slpsearchwrapper1">
-            <div className="slpsearchtype" />
-            <div className="slpsearchinput" />
+          <div className="slpsearchwrapper">
+              <SearchBar/>
           </div>
         </div>
         <div className="slpresult">
---
+++ front/src/PlayStation/PlayStageJS/SettingModal.js
@@ -0,0 +1,172 @@
+import React, { useEffect, useRef, useState } from 'react';
+import '../PlayStageCss/CreateStageModal.css';
+import {useNavigate} from "react-router-dom";
+import Cancel from '../PlayStageImage/Icon/Cancel.svg';
+import Create from '../PlayStageImage/Icon/Create.svg';
+import Logo from '../PlayStageImage/img/Logo.png';
+import '../PlayStageCss/SettingModal.css';
+
+
+function SettingModal({setModalOpen}) {
+ // const [address,setAdress] =('');
+ const [title,setTitle] = useState('');
+ const [desc,setDesc] = ('');
+ const [genre,setGenre] = useState('');
+ const [tag,setTag] = useState('');
+ const [pw,setPw] = useState('');
+ const navi = useNavigate();
+ const [img, setImg] = useState('');
+ const [disable,setDisable] = useState(true);
+ const [isPublic,setIsPublic]= useState(true);
+ //파일 업로드/미리보기 이벤트
+ const onUpload = (e)=>{
+     const file = e.target.files[0];
+     const reader = new FileReader();
+     reader.readAsDataURL(file);
+
+     return new Promise((resolve)=>{
+         reader.onload=()=>{
+             setImg(reader.result || null);//파일의 컨텐츠
+             resolve();
+         };
+     });
+ }
+ //Private일 경우 비밀번호 나오게 div 나오게 만들기
+
+ const OpenPw = (e)=> {
+     if(!isPublic){
+         setDisable(true);
+         setIsPublic(true);
+         setPw('');
+     }else{
+         setDisable(false);
+         setIsPublic(false);
+         console.log({setIsPublic})
+     } 
+ };
+ //모달 끄기
+ const closeModal = () => {
+     setModalOpen(false);
+ };
+ // const MakeStage = useNavigate("/PlayStage.js/${address}")
+ //모달 외부 클릭 시 끄기 처리
+ //Modal 창을 useRef로 취득
+ const modalRef = useRef(null);
+ useEffect(()=>{
+     //이벤트 핸들러 함수
+     const handler = (e) => {
+         //mousedown 이벤트가 발생한 영역이 모달창이 아닐 때 , 모달창 제거 처리.
+         if(modalRef.current && !modalRef.current.contains(e.target)){
+             setModalOpen(false);
+         }
+     };
+     //이벤트  핸들러 등록
+     document.addEventListener('mousedown',handler);
+     //모바일 버전
+     document.addEventListener('touchstart',handler);
+     return () =>{
+         //이벤트 핸들러 해제
+         document.removeEventListener('mousedown',handler);
+         document.removeEventListener('touchstart',handler);//모바일
+     };
+
+ });
+ //정보 기입 확인
+ const handleSubmit = (e) =>{
+     e.preventDefault();
+     //입력 필드값 검증
+     if(title === '' ||  desc ==='' || genre ==='' || tag ==='' || img === '' ){
+         alert('모든 정보를 기입해주세요');//입력필드가 하나라도 비어있을 시
+         console.log('success!');
+         return;
+     }else{
+      
+         setModalOpen(false);
+         console.log('Create success!')
+     }
+ }
+ return (
+     <div ref={modalRef} className="createstagemodal-parent">
+     <div className="createstagemodal">
+       <div className="createstagemodal-inner">
+         <div className="frame-child" />
+       </div>
+       <div className="wrapper">
+         <div className="div">
+           <div className="child" />
+           <form onSubmit={handleSubmit}>
+             <button type='submit' className='div1'>Stage 생성</button>                
+           </form>
+         </div>
+       </div>
+       {/* <a href='./PlayStage.js'>
+         <img className="createstagemodal-child" alt=""/>
+       </a> */}
+       <div className="findmodaltitle">
+         <div className="wepli">WEPLi</div>
+       </div>
+       <div className="Makestage">Stage 생성</div>
+       <div className="mypageemailmodal">
+         <div className="rectangle-parent">
+           <div className="group-child" />
+           <div className="div2">
+             <div class>
+                 <img width={'60%'} src={img} alt='' className='groupChildImage'/>
+             </div>
+             <label className="input-file-button" for="ImageUpload">
+                 업로드
+             </label>
+           <input accept='image/*' multiple type='file' id='ImageUpload' style={{display:'none'}}
+           onChange={e=>onUpload(e)}>
+           </input>
+           </div>
+         </div>
+         
+         <img
+           className="mypageemailmodal-child"
+           alt=""
+           src={Cancel}
+           onClick={closeModal}
+         />
+         
+         <div className="rectangle-group">
+           <div className="group-item" />
+           <div className="container">
+             <div className="div3">
+                 <input type='text' placeholder='스테이지 제목' value={title} onChange={(e)=>setTitle(e.target.value)}/>
+             </div>
+           </div>
+         </div>
+         <div className="rectangle-container">
+           <div className="group-inner" />
+           <div className="checkbox-wrapper">
+             <input type='text' className='checkboxPW' disabled={disable} placeholder='패스워드 입력'
+             value={pw} onChange={(e)=>setPw(e.target.value)}/>
+             <input type='checkbox' onClick={OpenPw}/>Private
+             {/* <div className="checkbox-">{`공개 비공개(checkBox)->비공개 암호설정`}</div> */}
+           </div>
+         </div>
+         <div className="group-div">
+           <div className="group-inner" />
+           <div className="frame">
+             <input type='text' className='div4' placeholder='태그입력' value={tag} onChange={(e)=>setTag(e.target.value)}/>
+           </div>
+         </div>
+         <div className="rectangle-parent1">
+           <div className="group-inner" />
+           <div className="frame">
+             <input type='text' className='frameGenre' placeholder='장르입력' value={genre} onChange={(e)=>setGenre(e.target.value)}/>
+           </div>
+         </div>
+       </div>
+       <img
+         className="wplieonlylogo-4-icon"
+         alt=""
+         src={Logo}
+       />
+     </div>
+   </div>
+    );
+}
+
+export default SettingModal;
