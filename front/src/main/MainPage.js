201c9989368bc91d33fe5eaf5a5bc2cadf45d220
helloa1109 <helloa1109@naver.com>
2023-07-31 01:11:04 +0900

    1. 메인페이지 디자인 스크롤 시 텍스트 fill 되게 수정

--- front/src/main/MainPage.js
+++ front/src/main/MainPage.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import "../App.css";
 
-import MainWrapper3 from "./MainWrapper3";
 import MainWrapper4 from "./MainWrapper4";
 import MainSection1 from "./MainSection1";
 import MainSection2 from "./MainSection2";
+import MainSection3 from "./MainSection3";
 
 function MainPage(props) {
     return (
         <div>
             <MainSection1/>
             <MainSection2/>
-            <MainWrapper3/>
-            <MainWrapper4/>
+            <MainSection3/>
+            {/*<MainWrapper4/>*/}
         </div>
     );
 
--- front/src/main/MainSection2.js
+++ front/src/main/MainSection2.js
@@ -5,14 +5,23 @@ import youtube from "./photo/youtube9.png";
 import youtube1 from "./photo/youtube10.png";
 
 function MainSection2(props) {
+
     return (
         <div className={'mainsection2'}>
-            <div className={'mainsection2photogroup'}>
-                <img src={youtube} className={'main2img'}/>
+            <div className={'mainsection2gra'}>
+                <div className={'mainsection2photogroup'}>
+                    <img src={youtube} className={'main2img'}/>
+                </div>
+                <div className={'mainsection2photogroup1'}>
+                    <img src={youtube1} className={'main2img2'}/>
+                </div>
             </div>
-            <div className={'mainsection2photogroup1'}>
-                <img src={youtube1} className={'main2img2'}/>
-            </div>
+            {/* New div for horizontal scrolling */}
+            {/*<div className={'mainsection2flow'}>*/}
+            {/*    <img src={handheart} className={'handheart'}/>*/}
+            {/*    <img src={handheart} className={'handheart1'}/>*/}
+            {/*    /!* Add more images here *!/*/}
+            {/*</div>*/}
         </div>
     );
 }
