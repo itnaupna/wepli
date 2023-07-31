69b98ef7b83271e8c3e69b131bee56caf50feecf
SeokjinKim <jameskim0208@gmail.com>
2023-07-31 16:25:18 +0900

    "20230731스테이지생성"

--- front/src/PlayStation/PlayStageJS/StageItemBig.js
+++ front/src/PlayStation/PlayStageJS/StageItemBig.js
@@ -6,9 +6,14 @@ import SLPMystagePlayingTitleIcon from '../PlayStageImage/Icon/SLPMystagePlaying
 import CreateStageModal from "./CreateStageModal.js";
 import CSM from "./CSM";
 import { Modal } from "@mui/material";
-
+import  Axios  from 'axios';
+import { useParams } from 'react-router-dom';
+import Wepli from '../../sidebar/photo/weplilogo.png';
 
 const StageItemBig = () => {
+  const [stageInfo,setStageInfo] = useState({});
+
+  const {userNick} = useParams();
   // 모달창 노출
   const [modalOpen, setModalOpen] = useState(false);
   const showModal = () => {
@@ -20,18 +25,43 @@ const StageItemBig = () => {
   const data =JSON.parse(sessionStorage.getItem("data"));
   const [checkStage, SetCheckStage] = useState(false);
 
-
+//세션스토리지 최신화
   useEffect(() => {
-    if (data&&data.stageaddress === null) {
-      SetCheckStage(true);
-    } else {
-      SetCheckStage(false);
-    }
-  }, [data]);
+    const fetchData = async () =>{
+      try{
+        const response = await fetch("/lv0/m/mypage");
+        const data = await response.json();
 
-  if(!data){
-    return null;
-  }
+        //업데이트
+        sessionStorage.setItem("data",JSON.stringify(data));
+
+        if(data && data.stageaddress === null){
+          SetCheckStage(true);
+        }else{
+          SetCheckStage(false);
+        }
+      }catch(error){
+        console.log('업데이트 실패',error);
+      }
+    };
+    fetchData();
+  },[]);
+//스테이지 정보를 불러오는 것이었던 것 
+  useEffect(()=>{
+    const fetchData = async ()=>{
+      try{
+        if(stageInfo !==null){
+        const response = await Axios.get('/lv0/s/stagehistory',{
+          params:{stageaddress:stageInfo},
+        });
+        setStageInfo(response.data);
+        }
+      }catch(error){
+        console.log('가져오는데 실패함 ㅠ',error);
+      }
+    };
+    fetchData();
+  },[stageInfo])
 
   return (
     <>
@@ -57,29 +87,31 @@ const StageItemBig = () => {
         <div className="slpitembig">
           <div className="slpitembigheader">
             <div className="slpitembigimgwrapper" >
-              <img
-                className="slpitembigimg-icon"
-                alt="스테이지썸네일"
-                src={null}
-              />
-              <div className="slpitembigday">생성일 : 2024-07-05</div>
+              {stageInfo && stageInfo.img !== null?(
+                <img
+                  className='slpitembigimg-icon'
+                  alt='썸네일'
+                  src={stageInfo.img}
+                />
+                ):(
+                  <img
+                  className='slpitembigimg-icon'
+                  alt="스테이지썸네일"
+                  src={Wepli}
+                  />
+                )}
+                {stageInfo &&(
+              <div className="slpitembigday">생성일 : {stageInfo.makeday}</div>
+              )}
             </div>
             <div className="slpitembiginfo">
-              <div className="slpitembiglikewrapper">
-                <img
-                  className="slpmystagelikeicon"
-                  alt=""
-                  src={SLPMystageLikeIcon}
-                />
-                <div className="slpmystagelikecount">1000</div>
-              </div>
               <div className="slpitembiglikewrapper">
                 <img
                   className="slpmystagepeopleicon"
                   alt=""
                   src={SLPMystagePeopleIcon}
                 />
-                <div className="slpmystagelikecount">1000</div>
+                <div className="slpmystagelikecount">{stageInfo.likes}</div>
               </div>
               <div className="slpitembiglikewrapper">
                 <img
@@ -87,21 +119,21 @@ const StageItemBig = () => {
                   alt=""
                   src={SLPMystageQIcon}
                 />
-                <div className="slpmystagelikecount">1000</div>
+                <div className="slpmystagelikecount">220</div>
               </div>
-              <div className="slpmystageowner">@만든이이름드가는자리</div>
+              <div className="slpmystageowner">@{stageInfo.nick}</div>
               <div className="slpmystagecategory">
-                #일이삼사오육칠팔구십일
+                #{stageInfo.tag}
               </div>
               <div className="slpmystagecategory">
-                #일이삼사오육칠팔구십일
+                #{stageInfo.genre}
               </div>
             </div>
           </div>
           <div className="slpitembigbody">
-            <div className="slpmystagetitle">내 스테이지 생성</div>
+            <div className="slpmystagetitle">내 스테이지 입장</div>
             <div className="slpmystagedescription">
-              나만의 스테이지를 가져보세요!
+                {stageInfo.desc}
             </div>
             <div className="slpmystageplayingtitlewrapper">
               <img
@@ -110,7 +142,7 @@ const StageItemBig = () => {
                 src={SLPMystagePlayingTitleIcon}
               />
               <div className="slpmystageplayingtitle">
-                재생곡 제목 드갑니다.
+                  {stageInfo.singer} - {stageInfo.title}
               </div>
             </div>
           </div>
