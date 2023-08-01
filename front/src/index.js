233bc95926bada919d9125841de869cdd5d8dcca
cyj <itnaupna@gmail.com>
2023-08-01 17:51:01 +0900

    끝.

--- front/src/index.js
+++ front/src/index.js
@@ -10,9 +10,7 @@ import { RecoilRoot } from 'recoil';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <RecoilRoot>
-    <div className="backgroundImgDiv">
       <App />
-    </div>
   </RecoilRoot>
 );
 
